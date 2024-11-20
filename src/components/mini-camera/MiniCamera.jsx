4e71d5f513a31617b8e3b6178078e65f2421e2b2
1ucyhan <lucehanblank@gmail.com>
2024-11-19 23:24:25 -0600

    after some attempt i still choose the 🐷

--- src/components/mini-camera/MiniCamera.jsx
+++ src/components/mini-camera/MiniCamera.jsx
@@ -6,7 +6,9 @@ export default function MiniCamera({name, image}) {
     return (
         <section className = "mini-cam">
             <section className = "cam-visual">
-                <img src = {image} alt = {name}/>
+                <section className = "overlay">
+                    <img src = {image} alt = {name}/>
+                </section>
                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><rect width={14} height={10} x={5} y={11} fill="white" rx={2}></rect><path fill="white" fillRule="evenodd" d="M9.5 6.75a.25.25 0 0 0-.25.25v5h-1.5V7c0-.966.784-1.75 1.75-1.75h5c.966 0 1.75.784 1.75 1.75v5h-1.5V7a.25.25 0 0 0-.25-.25z" clipRule="evenodd"></path></svg>
             </section>
             <p>{name}</p>
--- src/components/right-navbar/RightNavBar.jsx
+++ src/components/right-navbar/RightNavBar.jsx
@@ -2,14 +2,35 @@ import React from 'react';
 import './RightNavBar.css';
 import MiniCamera from '../mini-camera/MiniCamera.jsx'
 import PigImage from "../../assets/pig.jpg";
+import v1 from "../../assets/v1.webp";
+import v2 from "../../assets/v2.png";
+import v3 from "../../assets/v3.webp";
+
+const miniCameraData = [
+    {
+        name: "Lobby",
+        image: PigImage
+    },
+    {
+        name: "Main Entrance",
+        image: PigImage
+    },
+    {
+        name: "Side Entrance",
+        image: PigImage
+    },
+]
 
 export default function RightNavBar({numGuests, numDisplayed}) {
     return (
         <div className = "right">
             <section className = "cam-display">
-                <MiniCamera name= {"Lobby"} image= {PigImage}/>
-                <MiniCamera name= {"Lobby"} image= {PigImage}/>
-                <MiniCamera name= {"Lobby"} image= {PigImage}/>
+                {miniCameraData.map((camData) => {
+                    return (
+                        <MiniCamera name= {camData.name} image= {camData.image}/>
+                    )
+
+                })}
             </section>
             <hr />
             <div className = "count">
