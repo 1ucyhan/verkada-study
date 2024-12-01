ee5434510e614b92d585a765e4b7243dec1f97d8
1ucyhan <lucehanblank@gmail.com>
2024-12-01 15:36:41 -0600

    oops i just realized classname

--- src/App.js
+++ src/App.js
@@ -33,27 +33,30 @@ const data = {
 
 function App() {
   return (
-    <div class="app">
-      <div class="bg"> </div>
-      <div class = "preview">
-        <img class="preview" src={data.preview} alt="Preview Camera Shot" />
+    <div className="app">
+      <div className="bg"> </div>
+      <div className = "preview">
+        <img className="preview" src={data.preview} alt="Preview Camera Shot" />
+        <div className = "live">
+
+        </div>
       </div>
-      <div class="search">
+      <div className="search">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0s.41-1.08 0-1.49zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5S14 7.01 14 9.5S11.99 14 9.5 14" /></svg>
         <input type="text" placeholder="Search Events..."></input>
       </div>
-      <div class="border" />
-      <div class="events">
+      <div className="border" />
+      <div className="events">
         {data.events.map((event) => {
           return (
-            <div class="event-row">
-              <img class="event-img" src={event.image} alt={event.title} />
-              <div class="dude-info">
-                <div class="event-title">{event.title}</div>
-                <div class="event-subtitle">{event.subtitle}</div>
+            <div className="event-row">
+              <img className="event-img" src={event.image} alt={event.title} />
+              <div className="dude-info">
+                <div className="event-title">{event.title}</div>
+                <div className="event-subtitle">{event.subtitle}</div>
               </div>
               <div> {event.site} - {event.detail} </div>
-              <div class = "event-time"> {event.timestamp} </div>
+              <div className = "event-time"> {event.timestamp} </div>
             </div>
           )
         })}
