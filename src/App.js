59e8848bcffe42a541353055ae1b131f382e21cc
1ucyhan <lucehanblank@gmail.com>
2024-12-01 22:15:46 -0600

    wow

--- src/App.js
+++ src/App.js
@@ -30,9 +30,69 @@ const cameraData = [
   }
 ];
 
+const mailboxData = [
+  {
+    firstName: "Gabriel",
+    lastName: "Ong",
+    place: "The Mail Room",
+    number: "2",
+    personImage: GabImage,
+  },
+  {
+    firstName: "Kayla",
+    lastName: "Bui",
+    place: "The Mail Room",
+    number: "1",
+    personImage: KaylaImage,
+  },
+  {
+    firstName: "Nhi",
+    lastName: "Quach",
+    place: "The Mail Room",
+    number: "4",
+    personImage: NhiImage,
+  },
+  {
+    firstName: "Lucy",
+    lastName: "Han",
+    place: "The Mail Room",
+    number: "1",
+    personImage: MockImage2,
+  },
+  {
+    firstName: "Yu",
+    lastName: "Han",
+    place: "The Mail Room",
+    number: "6",
+    personImage: DadImage,
+  },
+  {
+    firstName: "Min",
+    lastName: "Shi",
+    place: "The Mail Room",
+    number: "1",
+    personImage: MomImage,
+  },
+];
+
+function displayNumBox(num) {
+  if (num > 1) {
+    return (
+      <div className = "boxes">
+        <b>{num}&nbsp;📦</b>
+      </div>
+    )
+  } else {
+    return (
+      <div style = {{fontSize: "20px"}} className = "box"> 📦 </div>
+    )
+  }
+}
+
 
 
 function App() {
+   
   return (
     <div className="app">
       <div className="title">
@@ -56,6 +116,22 @@ function App() {
       <div className = "border">
         <hr />
         Arrived Wed, Jun 7 2023
+      </div>
+      <div className = "mailboxes">
+        {mailboxData.map((mailbox) => {
+          return (
+            <div className = "mailbox">
+              <div className = "info">
+                <div className = "name">
+                {mailbox.firstName}&nbsp;<b>{mailbox.lastName}</b>
+                </div>
+                {mailbox.place}
+              </div>
+              <img src = {mailbox.personImage} alt = {mailbox.firstName}></img>
+              <div className = "numBox"> {displayNumBox(mailbox.number)}</div>
+            </div>
+          )
+        })}
       </div>
     </div>
 
